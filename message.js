e2bbbe6f7b20d1f05c88bfa1f1d1a45e727b72ff
dalenapoles <dalenapoles@gmail.com>
2026-07-09 21:25:37 +0800

    Accept 003

--- message.js
+++ message.js
@@ -5,6 +5,20 @@ let letter = document.getElementById("letter")
 let letterBG = document.getElementById("letterBackground")
 let happyContainer = document.getElementById("happyContainer");
 let holderImage = document.getElementById("holderImage")
+let icon1 = document.getElementById("icon1");
+let icon2 = document.getElementById("icon2");
+let icon3 = document.getElementById("icon3");
+
+
+    document.getElementById("icon3").addEventListener("click", () => {
+    window.location.href = "flower.html";
+  });
+    document.getElementById("icon2").addEventListener("click", () => {
+    window.location.href = "music.html";
+  });
+    document.getElementById("icon1").addEventListener("click", () => {
+    window.location.href = "message.html";
+  });
 
 
 closedEnv.addEventListener("click", function changeEnv() {
